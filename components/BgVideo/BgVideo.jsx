9c168ab4da9ef6de57a67c52e7b3e1950c283f9c
Felix Behne <felixbehne20@gmail.com>
2022-05-02 20:40:41 +0200

    :bug: Add poster for bgvideo

--- components/BgVideo/BgVideo.jsx
+++ components/BgVideo/BgVideo.jsx
@@ -1,16 +1,22 @@
-import PropTypes from 'prop-types';
 import styles from './BgVideo.module.css';
 
-/**
- * Fullscreen background video that is fully responsive
- */
-const BgVideo = ({ videoLocation, loop, controls, autoPlay, children }) => {
+const BgVideo = ({ children }) => {
   return (
     <section id="page-wrap" className={styles.showcase}>
       <div className={styles.videoContainer}>
-        <video width="100%" controls playsInline muted loop autoPlay>
+        <video
+          width="100%"
+          controls={false}
+          preload="auto"
+          playsInline
+          muted
+          loop
+          autoPlay
+          poster="/videos/poster.png"
+        >
           <source src="/videos/globe.mp4" type="video/mp4; codecs=hevc" />
           <source src="/videos/globe.webm" type="video/webm; codecs=vp9" />
+          You're browser doesn't support HTML5 videos
         </video>
       </div>
       {children}
@@ -19,30 +25,3 @@ const BgVideo = ({ videoLocation, loop, controls, autoPlay, children }) => {
 };
 
 export default BgVideo;
-
-BgVideo.propTypes = {
-  /**
-   * Where is your video located?
-   */
-  videoLocation: PropTypes.string.isRequired,
-  /**
-   * Should the video loop ?
-   */
-  loop: PropTypes.bool,
-  /**
-   * Should video controls be displayed ?
-   */
-  controls: PropTypes.bool,
-  /**
-   * Should the video automatically start to play  ?
-   */
-  autoPlay: PropTypes.bool
-};
-
-BgVideo.defaultProps = {
-  videoLocation:
-    'https://res.cloudinary.com/diz3ottxo/video/upload/v1606299408/minimal-globe_uardxq.mp4',
-  loop: true,
-  controls: false,
-  autoPlay: true
-};
