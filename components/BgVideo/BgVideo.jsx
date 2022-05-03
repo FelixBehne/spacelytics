6e877e8b157979ed987c476849189d18ef1cdb3a
Felix Behne <felixbehne20@gmail.com>
2022-05-03 17:20:14 +0200

    :ambulance: Fix bgvideo for mobile (again)

--- components/BgVideo/BgVideo.jsx
+++ components/BgVideo/BgVideo.jsx
@@ -6,16 +6,18 @@ const BgVideo = ({ children }) => {
     <section id="page-wrap" className={styles.showcase}>
       <div className={styles.videoContainer}>
         <video
-          controls={false}
           preload="auto"
+          src={'/videos/globe.mp4'}
+          type="video/mp4"
+          loop
+          controls={false}
+          autoPlay
           playsInline
           muted
-          loop
-          autoPlay
           poster="/videos/poster.png"
         >
-          <source src="/videos/globe.mp4" type="video/mp4; codecs=hevc" />
           <source src="/videos/globe.webm" type="video/webm; codecs=vp9" />
+          Your Browser does not support Html5 videos
         </video>
       </div>
       {children}
--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html style={{ overflow: 'hidden' }}>
+    <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
