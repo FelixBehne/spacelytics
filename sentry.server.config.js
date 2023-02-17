ecfc47143c0e4e40e26d74b3e4feb8eabe6dc18a
Felix Behne <felixbehne20@gmail.com>
2023-02-17 20:20:25 +0100

    fix: fix sentry config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,14 +7,11 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://556c3fd2d4504c2dae2668632f4bcee1@o4504696649220096.ingest.sentry.io/4504696650465280',
+  dsn: SENTRY_DSN || 'https://556c3fd2d4504c2dae2668632f4bcee1@o4504696649220096.ingest.sentry.io/4504696650465280',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  autoSessionTracking: true,
 });
