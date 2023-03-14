5fb0bacf75489e8cd8b68d1167d0c1ef3f958c34
Felix Behne <felixbehne20@gmail.com>
2023-03-14 12:36:45 +0100

    feature: add first version of globe

--- pages/starlink.tsx
+++ pages/starlink.tsx
@@ -1,15 +1,122 @@
 import { Box, Center } from '@chakra-ui/react';
-
+import createGlobe from 'cobe';
+import { ReactElement, useEffect, useRef } from 'react';
+import { useSpring } from 'react-spring';
 import { NextPageWithLayout } from './_app';
-import { ReactElement } from 'react';
+
 import AnalyticsLayout from '@/components/AnalyticsLayout';
-import NotImplemented from '@/components/NotImplemented';
 
 const StarlinkPage: NextPageWithLayout = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const pointerInteracting = useRef<number | null>(null);
+  const pointerInteractionMovement = useRef<number>(0);
+  const [{ r }, api] = useSpring<{ r: number }>(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+
+  useEffect(() => {
+    let width = 0;
+    let phi = 0;
+    const onResize = () => {
+      if (canvasRef.current) {
+        width = canvasRef.current.offsetWidth;
+        const devicePixelRatio = window.devicePixelRatio || 1;
+        canvasRef.current.width = width * devicePixelRatio;
+        canvasRef.current.height = width * devicePixelRatio;
+      }
+    };
+    window.addEventListener('resize', onResize);
+    onResize();
+
+    const globe = createGlobe(canvasRef.current!, {
+      devicePixelRatio: 2,
+      width: width * 2,
+      height: width * 2,
+      phi: 0,
+      theta: 0.3,
+      dark: 1,
+      diffuse: 3,
+      mapSamples: 160000,
+      mapBrightness: 1.2,
+      baseColor: [1, 1, 1],
+      markerColor: [251 / 255, 100 / 255, 21 / 255],
+      glowColor: [1.2, 1.2, 1.2],
+      markers: [],
+      onRender: (state) => {
+        // This prevents rotation while dragging
+        if (!pointerInteracting.current) {
+          // Called on every animation frame.
+          // `state` will be an empty object, return updated params.
+          phi += 0.001;
+        }
+        state.phi = phi + pointerInteractionMovement.current / 200;
+        state.width = width * 2;
+        state.height = width * 2;
+      },
+    });
+    setTimeout(() => {
+      if (canvasRef.current) {
+        canvasRef.current.style.opacity = '1';
+      }
+    });
+
+    return () => globe.destroy();
+  }, []);
+
   return (
-    <Box p='4'>
-      <Center h='70vh'>
-        <NotImplemented />
+    <Box p={['2', '4']} overflowX='auto'>
+      <Center>
+        <canvas
+          ref={canvasRef}
+          onPointerDown={(e) => {
+            pointerInteracting.current =
+              e.clientX - pointerInteractionMovement.current;
+            canvasRef.current!.style.cursor = 'grabbing';
+          }}
+          onPointerUp={() => {
+            pointerInteracting.current = null;
+            canvasRef.current!.style.cursor = 'grab';
+          }}
+          onPointerOut={() => {
+            pointerInteracting.current = null;
+            canvasRef.current!.style.cursor = 'grab';
+          }}
+          onMouseMove={(e) => {
+            if (pointerInteracting.current !== null) {
+              const delta = e.clientX - pointerInteracting.current;
+              pointerInteractionMovement.current = delta;
+              api.start({
+                r: delta / 200,
+              });
+            }
+          }}
+          onTouchMove={(e) => {
+            if (pointerInteracting.current !== null && e.touches[0]) {
+              const delta = e.touches[0].clientX - pointerInteracting.current;
+              pointerInteractionMovement.current = delta;
+              api.start({
+                r: delta / 100,
+              });
+            }
+          }}
+          style={{
+            width: '100%',
+            height: 'auto',
+            minHeight: '400px',
+            maxWidth: '650px',
+            aspectRatio: 1,
+            cursor: 'grab',
+            contain: 'layout paint size',
+            opacity: 0,
+            transition: 'opacity 1s ease',
+          }}
+        />
       </Center>
     </Box>
   );
